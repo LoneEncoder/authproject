71dc0ef69632cba70774442bd346d2a3a3bd1ad7
LoneEncoder <georgy.kozhushko@wesoftyou.com>
2022-10-05 22:05:16 +0300

    rename project

--- authproject/asgi.py
+++ authproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for auhproject project.
+ASGI config for authproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auhproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authproject.settings')
 
 application = get_asgi_application()
